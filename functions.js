b6ae816fff7d05f26b88ce617cd8f4f89e791378
Colyn Gremaud <colyn.gremaud@gmail.com>
2021-02-20 21:40:52 +0000

    Created .spec file for functions created a few functions.

--- functions.js
+++ functions.js
@@ -7,13 +7,37 @@ function createRoverInstance(position){
   return new Rover(position)
 }
 
-function promptUser(rover){
-  let userInput 
-  userInput = input.question("[P]: Update Position, [M]: Mode Change, [S]: Status Check, [Q]: Quit\nWhat will you do?: ")
-  return userInput.toUpperCase()
+updatePosition(newPosition){
+  // let newPosition = input.question("Enter new position: ")
+  let updatePosition = new Command("MOVE", newPosition)
+  return updatePosition 
 }
+
+function promptUser(rover){
+  let userCommand 
+  let numCommands 
+  let commandsArray
+  for (let i = 0; i < numCommands -1; i++)  
+    userCommand = input.question("[P]: Update Position, [M]: Mode Change, [S]: Status Check, [Q]: Quit\nWhat will you do?: ")
+    if (userCommand === "P"){
+      let userPosition = input.question("")
+      commandsArray.push(updatePosition(r))
+    } else if (userCommand === "M"){
+
+    } else if (userCommand === "S"){
+
+    } else if (userCommand === "Q"){
+
+    } else {
+      
+    }
+    //return userCommand.toUpperCase()
+}
+
 
 module.exports = {
   promptUser,
   createRoverInstance
 }
+
+//actually what I should do is create a loop that calls the prompt function for a given number of times and builds an array out of the user's inputs. 
---
+++ spec/functions.spec.js
@@ -0,0 +1,4 @@
+const Rover = require("./rover.js")
+const Message = require("./message.js")
+const Command = require("./command.js")
+const input = require('readline-sync')
